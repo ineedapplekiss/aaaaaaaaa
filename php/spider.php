4962a006676cecefe8358958bf8bf28b1a8b785f
shang <9212219@163.com>
2013-08-27 19:24:08 +0800

    add spider

---
+++ php/spider.php
@@ -0,0 +1,70 @@
+<?php
+
+$url = "http://www.dianping.com/search/category/1/0";
+
+echo uc_fopen($url);
+
+
+function uc_fopen($url, $limit = 0, $post = '', $cookie = '', $bysocket = FALSE, $ip = '', $timeout = 15, $block = TRUE) {
+	$return = '';
+	$matches = parse_url($url);
+	!isset($matches['host']) && $matches['host'] = '';
+	!isset($matches['path']) && $matches['path'] = '';
+	!isset($matches['query']) && $matches['query'] = '';
+	!isset($matches['port']) && $matches['port'] = '';
+	$host = $matches['host'];
+	$path = $matches['path'] ? $matches['path'].($matches['query'] ? '?'.$matches['query'] : '') : '/';
+	$port = !empty($matches['port']) ? $matches['port'] : 80;
+	if($post) {
+		$out = "POST $path HTTP/1.0\r\n";
+		$out .= "Accept: */*\r\n";
+		//$out .= "Referer: $boardurl\r\n";
+		$out .= "Accept-Language: zh-cn\r\n";
+		$out .= "Content-Type: application/x-www-form-urlencoded\r\n";
+		$out .= "User-Agent: $_SERVER[HTTP_USER_AGENT]\r\n";
+		$out .= "Host: $host\r\n";
+		$out .= 'Content-Length: '.strlen($post)."\r\n";
+		$out .= "Connection: Close\r\n";
+		$out .= "Cache-Control: no-cache\r\n";
+		$out .= "Cookie: $cookie\r\n\r\n";
+		$out .= $post;
+	} else {
+		$out = "GET $path HTTP/1.0\r\n";
+		//$out .= "Referer: $boardurl\r\n";
+		$out .= "User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:23.0) Gecko/20100101 Firefox/23.0\r\n";
+		$out .= "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n";
+		$out .= "Accept-Language: zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3\r\n";
+		$out .= "Accept-Encoding: gzip, deflate\r\n";
+		$out .= "Host: $host\r\n";
+		$out .= "Connection: keep-alive\r\n";
+		$out .= "Cookie: $cookie\r\n\r\n";
+	}
+	$fp = @fsockopen(($ip ? $ip : $host), $port, $errno, $errstr, $timeout);
+	if(!$fp) {
+		return '';//note $errstr : $errno \r\n
+	} else {
+		stream_set_blocking($fp, $block);
+		stream_set_timeout($fp, $timeout);
+		@fwrite($fp, $out);
+		$status = stream_get_meta_data($fp);
+		if(!$status['timed_out']) {
+			while (!feof($fp)) {
+				if(($header = @fgets($fp)) && ($header == "\r\n" ||  $header == "\n")) {
+					break;
+				}
+			}
+
+			$stop = false;
+			while(!feof($fp) && !$stop) {
+				$data = fread($fp, ($limit == 0 || $limit > 8192 ? 8192 : $limit));
+				$return .= $data;
+				if($limit) {
+					$limit -= strlen($data);
+					$stop = $limit <= 0;
+				}
+			}
+		}
+		@fclose($fp);
+		return $return;
+	}
+}
